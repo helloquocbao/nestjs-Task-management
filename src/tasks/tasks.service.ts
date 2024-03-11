7af400f4badcc343327fa42f8883100e9db39659
qbao <lehoquocao9@gmail.com>
2024-03-11 22:53:54 +0700

    create dto task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import { TaksStatus, Task } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaksStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
