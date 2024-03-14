06d1ab82e9c3266e83bc5e01835b06679ac23686
LHQBao <bao@sadec.asia>
2024-03-14 10:11:12 +0700

    update project

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { JwtPayload } from './jwt-payload.interface';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
+      secretOrKey: process.env.JWT_CECRET || config.get('jwt.secret'),
     });
   }
 
